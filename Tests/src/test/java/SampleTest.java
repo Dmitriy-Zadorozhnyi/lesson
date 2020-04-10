11f2f497cfcc9bfdaee78ec561435e0cb54078cf
Dmitriy <dmbitrbik29@gmail.com>
2020-04-10 19:24:07 +0300

    move SampleTest to Tests.src.test add Package Utility add ExecutionListener.class add ResultSender ResultSender sends results to InfluxDB

--- Tests/src/test/java/SampleTest.java
+++ Tests/src/test/java/SampleTest.java
@@ -26,17 +26,16 @@ public class SampleTest {
         driver = new ChromeDriver();
         logger.info("Драйвер поднят");
     }
-    @Test(description = "open page")
+    @Test(description = "openPage")
     public void openPage() {
         driver.get("https://otus.ru/");
         logger.info("Открыта страница отус");
     }
 
-    @Test(description = "login", dependsOnMethods = "open page")
+    @Test(description = "login", dependsOnMethods = "openPage")
     public void login(){
         driver.findElement(By.className("header2__auth js-open-modal")).click();
         driver.findElement(By.partialLinkText("/login/google-oauth2/")).click();
-        driver.findElement(By.partialLinkText("/logout/"));
         logger.info("Пользователь авторизовался");
     }
 
