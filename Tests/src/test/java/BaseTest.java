a282022ffc6d91138f24b734ba434e0b15c95961
Дмитрий <dmbitrbik29@gmail.com>
2020-04-20 18:33:39 +0300

    added selenoid remoteWebDriver to ConfigProperty

--- Tests/src/test/java/BaseTest.java
+++ Tests/src/test/java/BaseTest.java
@@ -2,12 +2,18 @@ import DriverManager.WebDriverFactory;
 import Utility.ConfigProperty;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 
 public abstract class BaseTest {
     public Logger logger = LogManager.getLogger(BaseTest.class);
     protected WebDriver driver;
+    protected WebElement element;
 
     @BeforeClass
     public void beforeClass() {
@@ -19,5 +25,6 @@ public abstract class BaseTest {
     public void afterClass() {
         WebDriverFactory.getInstance().getDriver().quit();
     }
+
 
 }
--- Tests/src/test/java/SampleTest.java
+++ Tests/src/test/java/SampleTest.java
@@ -4,21 +4,38 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 
 @Listeners(ExecutionListener.class)
 public class SampleTest extends BaseTest {
 
     @Test(description = "openPage")
     public void openPage() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://otus.ru/");
         logger.info("Открыта страница отус");
     }
 
-    @Test(description = "login", dependsOnMethods = "openPage")
-    public void login(){
-        driver.findElement(By.className("header2__auth")).click();
-        driver.findElement(By.className("new-ic-google-white-square")).click();
-        logger.info("Пользователь авторизовался");
+    @Test(description = "switchProgramming", dependsOnMethods = "openPage")
+    public void program(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[@href=\"/categories/programming/\"]")).click();
+    }
+    @Test(description = "switchOperations", dependsOnMethods = "program")
+    public void operations(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[@href=\"/categories/operations/\"]")).click();
+    }
+    @Test(description = "switchInformation-security", dependsOnMethods = "operations")
+    public void informationSecurity(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[@href=\"/categories/information-security/\"]")).click();
+    }
+    @Test(description = "switchData-science", dependsOnMethods = "informationSecurity")
+    public void dataScience(){
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[@href=\"/categories/data-science/\"]")).click();
     }
 
 }
